c4bcedfe9f2fd7e6645accc69a7e1cd3d49b821b
bandanasinghhlr-cell <bandanasingh.hlr@gmail.com>
2026-07-10 10:40:37 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -2,10 +2,10 @@
 
 echo "Welcome to the Jenkins Hello 😄"
 
-echo "User : $(whoami)"
-echo "Hostname : $(hostname)"
-echo "Current Directory : $(pwd)"
-echo "Date : $(date)"
+echo "User: $(whoami)"
+echo "Hostname: $(hostname)"
+echo "Current Directory: $(pwd)"
+echo "Date: $(date)"
 
 sudo apt update
 sudo apt install -y tree
